4505d4d05fda47573b99200646a50ba623ecacde
Tom Coleman <tom@chromatic.com>
2022-12-09 10:10:30 +1100

    Upgrade Storybook to v7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
 module.exports = {
-    extends: [
-        '@grozav',
-        '@vue/typescript/recommended',
-        'plugin:vue/vue3-recommended'
-    ],
-    plugins: [
-        'vue'
-    ],
-    rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        'vue/html-indent': ['warn', 4],
-        'vue/max-attributes-per-line': 'off',
-        'vue/custom-event-name-casing': 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/require-prop-types': 'off',
-        'vue/comment-directive': 'off'
-    }
+  extends: ['@grozav', '@vue/typescript/recommended', 'plugin:vue/vue3-recommended', 'plugin:storybook/recommended'],
+  plugins: ['vue'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/html-indent': ['warn', 4],
+    'vue/max-attributes-per-line': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/comment-directive': 'off'
+  }
 };
